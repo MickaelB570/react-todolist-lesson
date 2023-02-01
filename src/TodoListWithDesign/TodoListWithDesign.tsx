b231649a66d3f59067ad29f16abf3b3eaf7236ef
MickaelB570 <113980499+MickaelB570@users.noreply.github.com>
2023-02-01 14:04:37 +0100

    maj

--- src/TodoListWithDesign/TodoListWithDesign.tsx
+++ src/TodoListWithDesign/TodoListWithDesign.tsx
@@ -7,44 +7,27 @@ import './TodoListWithDesign.css';
 interface ListItemCateg {
     id: string;
     label: string;
-    item: ListItem[];
+    items: ListItem[];
 }
 
 
 interface ListItem {
     id: string;
-    label: string;
+    title: string;
 }
 
 const TodoListWithDesign = () => {
-    const [todo, setTodo] = useState<ListItem[]>([]);
-    const [inProgress, setInProgress] = useState<ListItem[]>([]);
-    const [done, setDone] = useState<ListItem[]>([]);
     const [item, setItem] = useState<ListItem[]>([]);
     const [itemCategorie, setItemCagetorie] = useState<ListItemCateg[]>([]);
     const [newCategName, setNewCategName] = useState<string>('');
     const [newItemName, setNewItemName] = useState<string>('');
-    const [newItemCategory, setNewItemCategory] = useState<string>('todo');
+    const [selectValeur, setSelectValeur] = useState<string>('');
 
-    const data = [
-        {
-            title: 'Ant Design Title 1',
-        },
-        {
-            title: 'Ant Design Title 2',
-        },
-        {
-            title: 'Ant Design Title 3',
-        },
-        {
-            title: 'Ant Design Title 4',
-        },
-    ];
+    const handleChange = (e: string) => {
+        setSelectValeur(e);
+        console.log(e);
 
-    const handleChange = (value: string) => {
-        console.log(`selected ${value}`);
     };
-
 
     const randomId = () => (Math.random() + 1).toString(36).substring(7);
 
@@ -55,196 +38,72 @@ const TodoListWithDesign = () => {
     const handleOnCategChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setNewCategName(e.target.value);
     };
-
-    const handleOnCategoryChange = (
-        e: React.ChangeEvent<HTMLSelectElement>
-    ) => {
-        setNewItemCategory(e.target.value);
-    };
     
     const handleOnCategClick = () => {
         const newCateg = {
             id: randomId(),
             label: newCategName,
-            item : item,
+            items : [],
         };
-
+        
         setItemCagetorie([...itemCategorie, newCateg] );
 
         setNewCategName('');
     };
 
-
     const handleOnClick = () => {
+        const newItemCategorie = [...itemCategorie];
         const newItem = {
             id: randomId(),
-            label: newItemName,
+            title : newItemName,
         };
-
-        switch (newItemCategory) {
-            case 'todo':
-                setTodo([...todo, newItem]);
-                break;
-            case 'inProgress':
-                setInProgress([...inProgress, newItem]);
-                break;
-            case 'done':
-                setDone([...done, newItem]);
-                break;
+    
+        const index = newItemCategorie.findIndex(({id}) => id === selectValeur);
+     
+        if(index !== -1) {
+            newItemCategorie[index].items.push(newItem);
+            setItemCagetorie(newItemCategorie);
+            setNewItemName('');
         }
-
-        setNewItemName('');
-        setNewItemCategory('todo');
     };
 
     return (
-        /*<div className="todo-list-basic">
-            <div className="todo-list-basic-add">
-                <input onChange={handleOnNameChange} value={newItemName} />
-                <select
-                    onChange={handleOnCategoryChange}
-                    value={newItemCategory}
-                >
-                    <option value="todo">To do</option>
-                    <option value="inProgress">In progress</option>
-                    <option value="done">Done</option>
-                </select>
-                <button disabled={!newItemName.length} onClick={handleOnClick}>
-                    Add to list
-                </button>
-            </div>
-
-            <div className="todo-list-basic-columns">
-                <div>
-                    <span>To do</span>
-                    <ul>
-                        {todo.map(({ id, label }) => (
-                            <li key={id}>{label}</li>
-                        ))}
-                    </ul>
-                </div>
-
-                <div>
-                    <span>In progress</span>
-                    <ul>
-                        {inProgress.map(({ id, label }) => (
-                            <li key={id}>{label}</li>
-                        ))}
-                    </ul>
-                </div>
-
-                <div>
-                    <span>Done</span>
-                    <ul>
-                        {done.map(({ id, label }) => (
-                            <li key={id}>{label}</li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        </div>
-    */
         <div>
             <div className='create-item'>
                 <Input onChange={handleOnCategChange} value={newCategName}  style={{}} size="large" placeholder="Basic usage" />
                 <Button onClick={handleOnCategClick} style={{}} type="primary" size={"large"}> Add Column </Button>
             </div>
             <div className='add-item'>
-                <Input style={{}} size="large" placeholder="Basic usage" />
+                <Input onChange={handleOnNameChange} value={newItemName} style={{}} size="large" placeholder="Basic usage" />
                 <Select
-                    defaultValue=""
                     style={{}}
                     size="large"
-                    onChange={handleChange}
-                    /*options={[
-                        { value: 'jack', label: 'Jack' },
-                        { value: 'lucy', label: 'Lucy' },
-                        { value: 'Yiminghe', label: 'yiminghe' },
-                        { value: 'disabled', label: 'Disabled', disabled: true },
-                    ]}*/
-                    
-                    options={
-                        itemCategorie.map(({id,label}) => ( { value : {id}, label : {label} } ))
-                    }
-
-                    //{todo.map(({ id, label }) => (
-                        //<li key={id}>{label}</li>
-                        //))}
+                    onSelect={handleChange}
+                    options={[
+                        {value: "", label: "Select column"},
+                        ...itemCategorie.map(laCateg  => ( { value : laCateg.id, label : laCateg.label } ))
+                    ]}
                 />
-                <Button style={{}} type="primary" size={"large"}> Add Item </Button>
+                <Button onClick={handleOnClick} style={{}} type="primary" size={"large"}> Add Item </Button>
+
             </div>
             <div className='show-items'>
-                <div>
+                {itemCategorie.map(({ id, label, items }) => (
+                    <div>
                     <List
                         itemLayout="horizontal"
-                        header="Exemples"
-                        dataSource={data}
-                        renderItem={(item) => (
+                        header={label}
+                        dataSource={items}
+                        renderItem={(items) => (
                             <List.Item>
                                 <List.Item.Meta
-                                    title={<a href="https://ant.design">{item.title}</a>}
+                                    title={<a href="https://ant.design" key={id}>{items.title}</a>}
                                 />
                             </List.Item>
                         )}
                     />
                 </div>
-                <div>
-                    <List                        
-                        header="Exemples"
-                        itemLayout="horizontal"
-                        dataSource={data}
-                        renderItem={(item) => (
-                            <List.Item>
-                                <List.Item.Meta
-                                    title={<a href="https://ant.design">{item.title}</a>}
-                                />
-                            </List.Item>
-                        )}
-                    />
-                </div>
-                <div>
-                    <List
-                        header="Exemples"
-                        itemLayout="horizontal"
-                        dataSource={data}
-                        renderItem={(item) => (
-                            <List.Item>
-                                <List.Item.Meta
-                                    title={<a href="https://ant.design">{item.title}</a>}
-                                />
-                            </List.Item>
-                        )}
-                    />
-                </div>
-                <div>
-                    <List
-                        header="Exemples"
-                        itemLayout="horizontal"
-                        dataSource={data}
-                        renderItem={(item) => (
-                            <List.Item>
-                                <List.Item.Meta
-                                    title={<a href="https://ant.design">{item.title}</a>}
-                                />
-                            </List.Item>
-                        )}
-                    />
-                </div>
-                <div>
-                    <List
-                        header="Exemples"
-                        itemLayout="horizontal"
-                        dataSource={data}
-                        renderItem={(item) => (
-                            <List.Item>
-                                <List.Item.Meta
-                                    title={<a href="https://ant.design">{item.title}</a>}
-                                />
-                            </List.Item>
-                        )}
-                    />
-                </div>
-                
+                ))}
             </div>
         </div>
     );
